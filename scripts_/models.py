afa7f4dbb7e87622e5fd74609b289e5b735f276f
Teddy <teddylnk1@gmail.com>
2022-05-21 19:36:20 +0300

    updated models

--- scripts_/models.py
+++ scripts_/models.py
@@ -6,7 +6,7 @@ import scipy.stats as stat
 from sklearn.tree import DecisionTreeClassifier 
 from sklearn import metrics
 from sklearn.model_selection import KFold
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, accuracy_score, precision_score, recall_score, confusion_matrix
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 from urllib.parse import urlparse
@@ -28,6 +28,32 @@ def mean_absolute_error(act, pred):
     mae = mean_absolute_error(act, pred)
     return mae
 
+def get_metrics(act, pred):
+        acc = accuracy_score(act, pred)
+        prec = precision_score(act, pred)
+        recall = recall_score(act, pred)
+        confusion = True
+        try:
+            cm = confusion_matrix(act, pred)
+            true_pos = cm[0][0]
+            true_neg = cm[1][1]
+            false_pos = cm[0][1]
+            false_neg = cm[1][0]
+        except:
+            print("Unable to calculate confusion matrix")
+            confusion = False
+        metric = {
+            'accuracy': round(acc, 2),
+            'precision': round(prec, 2),
+            'recall': round(recall, 2)
+        }
+        if confusion:
+            metric['true_neg'] = true_neg
+            metric['true_pos'] = true_pos
+            metric['false_neg'] = false_neg
+            metric['false_pos'] = false_pos
+        
+        return metric
 
 
 class DecisionTreeModel:
@@ -44,7 +70,7 @@ class DecisionTreeModel:
         self.model = DecisionTreeClassifier(max_depth=4)
 
         
-    def train(self, folds=1):
+    def train(self, folds=1,experiment_name=""):
         
         kf = KFold(n_splits = folds)
         
@@ -56,14 +82,15 @@ class DecisionTreeModel:
         loss_dict['mae'] = []
         loss_dict['rmse'] = []
         loss_dict['mse'] = []
-
+        mlflow.set_experiment(experiment_name)
         for i in range(folds):
             train_index, valid_index = next(iterator)
             
             X_train, y_train = self.X_train.iloc[train_index], self.y_train.iloc[train_index]
             X_valid, y_valid = self.X_train.iloc[valid_index], self.y_train.iloc[valid_index]
 
-            with mlflow.start_run():
+            with mlflow.start_run(run_name=f"{i} fold training"):
+                mlflow.sklearn.autolog()
                 self.model = self.model.fit(X_train, y_train)
                 
                 vali_pred = self.model.predict(X_valid)
@@ -75,8 +102,8 @@ class DecisionTreeModel:
                 # loss_dict['mae'].append(mean_absolute_error(y_valid, vali_pred))
                 # loss_dict['rmse'].append(root_mean_squared_error(y_valid, vali_pred))
                 # loss_dict['mse'].append(mean_squared_error(y_valid, vali_pred))
-                mlflow.log_metric("rmse", loss)
-                mlflow.log_metric("r2", r2_score(y_valid, vali_pred))
+                # mlflow.log_metric("rmse", loss)
+                # mlflow.log_metric("r2", r2_score(y_valid, vali_pred))
                 # mlflow.log_metric("mae", mean_absolute_error(y_valid, vali_pred))
                 
                 self.__printAccuracy(accuracy, i, label="Validation")
@@ -88,17 +115,17 @@ class DecisionTreeModel:
                 tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
                 # Model registry does not work with file store
-                if tracking_url_type_store != "file":
+                # if tracking_url_type_store != "file":
 
-                    # Register the model
-                    # There are other ways to use the Model Registry, which depends on the use case,
-                    # please refer to the doc for more information:
-                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                    mlflow.sklearn.log_model(self.model, "model", registered_model_name="DecisionTreeModel")
-                else:
-                    mlflow.sklearn.log_model(self.model, "model")
+                #     # Register the model
+                #     # There are other ways to use the Model Registry, which depends on the use case,
+                #     # please refer to the doc for more information:
+                #     # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                #     mlflow.sklearn.log_model(self.model, "model", registered_model_name="DecisionTreeModel")
+                # else:
+                #     mlflow.sklearn.log_model(self.model, "model")
 
-            
+        #  self.log_model()   
         return self.model, acc_arr, loss_arr
     
     def test(self):
@@ -121,8 +148,13 @@ class DecisionTreeModel:
         
         fi_df['feature'] = self.X_train.columns.to_list()
         fi_df['feature_importances'] = importance
+        figure = plt.figure(figsize=(10,10))
+        ax = sns.barplot(x=fi_df['feature'], y=fi_df['feature_importances'])
+        ax.set_title('Feature Importance')
+        ax.set_xlabel("Features", fontsize=18)
+        ax.set_ylabel("Feature Importance", fontsize=18)
         
-        return fi_df
+        return fi_df, figure
     
     def __printAccuracy(self, acc, step=1, label=""):
         print(f"step {step}: {label} Accuracy of DecisionTreesModel is: {acc:.3f}")
@@ -140,11 +172,42 @@ class DecisionTreeModel:
         return metrics.classification_report(pred, actual)
     
     def confusion_matrix(self, pred, actual):
+        figure = plt.figure(figsize=(10, 10))
         ax=sns.heatmap(pd.DataFrame(metrics.confusion_matrix(pred, actual)))
         plt.title('Confusion matrix')
         plt.ylabel('Actual')
         plt.xlabel('Predicted')
-        return metrics.confusion_matrix(pred, actual)
+        return metrics.confusion_matrix(pred, actual),figure
+
+    def log_model(self, x_test,y_test, experiment_name, run_name, model_name, run_params=None):
+        pred = self.model.predict(x_test)
+        run_metrics = get_metrics(y_test, pred)
+        feature_importance, feature_importance_plot = self.get_feature_importance()
+        _, cm_plot = self.confusion_matrix(pred, y_test)
+
+
+        mlflow.set_experiment(experiment_name)
+        mlflow.set_tracking_uri('http://localhost:5000')
+        with mlflow.start_run(run_name=run_name):
+            if run_params:
+                for name in run_params:
+                    mlflow.log_param(name, run_params[name])
+            for name in run_metrics:
+                mlflow.log_metric(name, run_metrics[name])
+
+            mlflow.log_param("columns", x_test.columns.to_list())
+            mlflow.log_figure(cm_plot, "confusion_matrix.png")
+            mlflow.log_figure(feature_importance_plot,
+                              "feature_importance.png")
+            cm_plot.savefig("./images/confusion_matrix.png")
+            feature_importance_plot.savefig("./images/feature_importance.png")
+            mlflow.log_dict(feature_importance.to_json(), "feature_importance.json")
+
+            mlflow.sklearn.log_model(
+                sk_model=self.model, artifact_path='models', registered_model_name=model_name)
+            print('Run - %s is logged to Experiment - %s' %
+                (run_name, experiment_name))
+        return run_metrics
 
 
 
@@ -161,7 +224,7 @@ class XGBClassifierModel:
 
 
         
-    def train(self, folds=1):
+    def train(self, folds=1, experiment_name=""):
         
         kf = KFold(n_splits = folds)
         
@@ -169,28 +232,31 @@ class XGBClassifierModel:
         
         loss_arr = []
         acc_arr = []
-
+        mlflow.set_experiment(experiment_name)
         for i in range(folds):
             train_index, valid_index = next(iterator)
             
             X_train, y_train = self.X_train.iloc[train_index], self.y_train.iloc[train_index]
             X_valid, y_valid = self.X_train.iloc[valid_index], self.y_train.iloc[valid_index]
+
+            with mlflow.start_run(run_name=f"{i} fold training"):
+                mlflow.sklearn.autolog()
                         
-            self.model = self.model.fit(X_train, y_train)
-            
-            vali_pred = self.model.predict(X_valid)
-            
-            accuracy = self.calculate_score(y_valid
-                                              , vali_pred)
-            
-            loss = loss_function(y_valid, vali_pred)
-            
-            self.__printAccuracy(accuracy, i, label="Validation")
-            self.__printLoss(loss, i, label="Validation")
-            print()
-            
-            acc_arr.append(accuracy)
-            loss_arr.append(loss)
+                self.model = self.model.fit(X_train, y_train)
+                
+                vali_pred = self.model.predict(X_valid)
+                
+                accuracy = self.calculate_score(y_valid
+                                                , vali_pred)
+                
+                loss = loss_function(y_valid, vali_pred)
+                
+                self.__printAccuracy(accuracy, i, label="Validation")
+                self.__printLoss(loss, i, label="Validation")
+                print()
+                
+                acc_arr.append(accuracy)
+                loss_arr.append(loss)
 
             
         return self.model, acc_arr, loss_arr
@@ -215,8 +281,13 @@ class XGBClassifierModel:
         
         fi_df['feature'] = self.X_train.columns.to_list()
         fi_df['feature_importances'] = importance
+        figure = plt.figure(figsize=(10,10))
+        ax = sns.barplot(x=fi_df['feature'], y=fi_df['feature_importances'])
+        ax.set_title('Feature Importance')
+        ax.set_xlabel("Features", fontsize=18)
+        ax.set_ylabel("Feature Importance", fontsize=18)
         
-        return fi_df
+        return fi_df, figure
     
     def __printAccuracy(self, acc, step=1, label=""):
         print(f"step {step}: {label} Accuracy of GradientBoostingClassifier is: {acc:.3f}")
@@ -234,11 +305,42 @@ class XGBClassifierModel:
         return metrics.classification_report(pred, actual)
     
     def confusion_matrix(self, pred, actual):
+        figure = plt.figure(figsize=(10, 10))
         ax=sns.heatmap(pd.DataFrame(metrics.confusion_matrix(pred, actual)))
         plt.title('Confusion matrix')
         plt.ylabel('Actual')
         plt.xlabel('Predicted')
-        return metrics.confusion_matrix(pred, actual)
+        return metrics.confusion_matrix(pred, actual),figure
+
+    def log_model(self, x_test,y_test, experiment_name, run_name, model_name, run_params=None):
+        pred = self.model.predict(x_test)
+        run_metrics = get_metrics(y_test, pred)
+        feature_importance, feature_importance_plot = self.get_feature_importance()
+        _, cm_plot = self.confusion_matrix(pred, y_test)
+
+
+        mlflow.set_experiment(experiment_name)
+        mlflow.set_tracking_uri('http://localhost:5000')
+        with mlflow.start_run(run_name=run_name):
+            if run_params:
+                for name in run_params:
+                    mlflow.log_param(name, run_params[name])
+            for name in run_metrics:
+                mlflow.log_metric(name, run_metrics[name])
+
+            mlflow.log_param("columns", x_test.columns.to_list())
+            mlflow.log_figure(cm_plot, "confusion_matrix.png")
+            mlflow.log_figure(feature_importance_plot,
+                              "feature_importance.png")
+            cm_plot.savefig("./images/confusion_matrix.png")
+            feature_importance_plot.savefig("./images/feature_importance.png")
+            mlflow.log_dict(feature_importance.to_json(), "feature_importance.json")
+
+            mlflow.sklearn.log_model(
+                sk_model=self.model, artifact_path='models', registered_model_name=model_name)
+            print('Run - %s is logged to Experiment - %s' %
+                (run_name, experiment_name))
+        return run_metrics
 
 
 class LogisticRegressionModel:
@@ -254,7 +356,7 @@ class LogisticRegressionModel:
         self.model = LogisticRegression()
 
         
-    def train(self, folds=1):
+    def train(self, folds=1, experiment_name=""):
         
         kf = KFold(n_splits = folds)
         
@@ -262,9 +364,8 @@ class LogisticRegressionModel:
         
         loss_arr = []
         acc_arr = []
-        model_name= self.model_name
-#         mlflow.end_run()
 
+        mlflow.set_experiment(experiment_name)
         for i in range(folds):
 
             train_index, valid_index = next(iterator)
@@ -272,19 +373,22 @@ class LogisticRegressionModel:
             X_train, y_train = self.X_train.iloc[train_index], self.y_train.iloc[train_index]
             X_valid, y_valid = self.X_train.iloc[valid_index], self.y_train.iloc[valid_index]
 
-            self.model = self.model.fit(X_train, y_train)
+            with mlflow.start_run(run_name=f"{i} fold training"):
+                mlflow.sklearn.autolog()
 
-            vali_pred = self.model.predict(X_valid)
+                self.model = self.model.fit(X_train, y_train)
 
-            accuracy = self.calculate_score(y_valid, vali_pred)
-            loss = loss_function(y_valid, vali_pred)
+                vali_pred = self.model.predict(X_valid)
 
-            self.__printAccuracy(accuracy, i, label="Validation")
-            self.__printLoss(loss, i, label="Validation")
-            print()
+                accuracy = self.calculate_score(y_valid, vali_pred)
+                loss = loss_function(y_valid, vali_pred)
 
-            acc_arr.append(accuracy)
-            loss_arr.append(loss)
+                self.__printAccuracy(accuracy, i, label="Validation")
+                self.__printLoss(loss, i, label="Validation")
+                print()
+
+                acc_arr.append(accuracy)
+                loss_arr.append(loss)
             
         return self.model, acc_arr, loss_arr
     
@@ -308,6 +412,20 @@ class LogisticRegressionModel:
     
     def calculate_score(self, pred, actual):
         return metrics.accuracy_score(actual, pred)
+
+    def get_feature_importance(self):
+        importance = self.model.coef_[0]
+        fi_df = pd.DataFrame()
+        
+        fi_df['feature'] = self.X_train.columns.to_list()
+        fi_df['feature_importances'] = importance
+        figure = plt.figure(figsize=(10,10))
+        ax = sns.barplot(x=fi_df['feature'], y=fi_df['feature_importances'])
+        ax.set_title('Feature Importance')
+        ax.set_xlabel("Features", fontsize=18)
+        ax.set_ylabel("Feature Importance", fontsize=18)
+        
+        return fi_df, figure
     
     def report(self, pred, actual):
         print("Test Metrics")
@@ -316,11 +434,12 @@ class LogisticRegressionModel:
         return metrics.classification_report(pred, actual)
     
     def confusion_matrix(self, pred, actual):
+        figure = plt.figure(figsize=(10, 10))
         ax=sns.heatmap(pd.DataFrame(metrics.confusion_matrix(pred, actual)))
         plt.title('Confusion matrix')
         plt.ylabel('Actual')
         plt.xlabel('Predicted')
-        return metrics.confusion_matrix(pred, actual)
+        return metrics.confusion_matrix(pred, actual),figure
     
     def get_p_values(self):
         """ 
@@ -358,3 +477,33 @@ class LogisticRegressionModel:
 
         plt.show()
         return fig
+
+    def log_model(self, x_test,y_test, experiment_name, run_name, model_name, run_params=None):
+        pred = self.model.predict(x_test)
+        run_metrics = get_metrics(y_test, pred)
+        feature_importance, feature_importance_plot = self.get_feature_importance()
+        _, cm_plot = self.confusion_matrix(pred, y_test)
+
+
+        mlflow.set_experiment(experiment_name)
+        mlflow.set_tracking_uri('http://localhost:5000')
+        with mlflow.start_run(run_name=run_name):
+            if run_params:
+                for name in run_params:
+                    mlflow.log_param(name, run_params[name])
+            for name in run_metrics:
+                mlflow.log_metric(name, run_metrics[name])
+
+            mlflow.log_param("columns", x_test.columns.to_list())
+            mlflow.log_figure(cm_plot, "confusion_matrix.png")
+            mlflow.log_figure(feature_importance_plot,
+                              "feature_importance.png")
+            cm_plot.savefig("./images/confusion_matrix.png")
+            feature_importance_plot.savefig("./images/feature_importance.png")
+            mlflow.log_dict(feature_importance.to_json(), "feature_importance.json")
+
+            mlflow.sklearn.log_model(
+                sk_model=self.model, artifact_path='models', registered_model_name=model_name)
+            print('Run - %s is logged to Experiment - %s' %
+                (run_name, experiment_name))
+        return run_metrics
